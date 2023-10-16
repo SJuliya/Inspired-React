2cb61982bb7fecbb6bb9206eabf10c45491cad8a
SJuliya <anjee2007@mail.ru>
2023-10-16 15:15:46 +0300

    order modal

--- src/Components/CartPage/CartPage.jsx
+++ src/Components/CartPage/CartPage.jsx
@@ -3,10 +3,12 @@ import {Order} from "./Order/Order";
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect, useState} from "react";
 import {fetchAll} from "../../features/goodsSlice";
+import {OrderModal} from "./OrderModal/OrderModal";
 
 export const CartPage = () => {
     const {cartItems, countItems} = useSelector(state => state.cart);
     const {goodsList} = useSelector(state => state.goods);
+    const {orderStatus} = useSelector(state => state.cart);
     const [count, setCount] = useState(0);
     const dispatch = useDispatch();
     useEffect(() => {
@@ -19,7 +21,8 @@ export const CartPage = () => {
     return (
         <>
             <Cart cartItems={cartItems} goodsList={goodsList} />
-            <Order cartItems={cartItems} goodsList={goodsList} />
+            {!!goodsList.length && <Order cartItems={cartItems}/>}
+            {orderStatus === 'success' && <OrderModal />}
         </>
     )
 }
