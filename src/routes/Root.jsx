bd3ddf47fed11db2f382cc8822b76c8c86ab0fc2
SJuliya <anjee2007@mail.ru>
2023-10-18 16:55:33 +0300

    change path in Root

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -2,7 +2,7 @@ import {Outlet} from 'react-router-dom';
 import {Header} from "../Components/Header/Header";
 import {Main} from "../Components/Layout/Main/Main";
 import {Footer} from "../Components/Footer/Footer";
-import ScrollToTop from "../../inspired-react/src/ScrollTop";
+import ScrollToTop from "../../src/ScrollTop";
 
 export const Root = () => {
   return (
